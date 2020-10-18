9c0df4cf50cefb04119ee4a09b8b7ffa2f7cf50e
Henriquejhap <jhaphenrique@gmail.com>
2020-10-18 17:08:03 -0300

    :package: Criação da LandingPage

--- web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App() {
-  return (
-    <h1>Rafaela Fotografias</h1>
-  );
-}
-
-export default App;
---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+
+import logoImg from '../images/logo.svg';
+import bonecosImg from '../images/bonecos-lading.svg';
+import cercaImg from '../images/cerca-lading.svg';
+
+import '../styles/pages/landing.css';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <header>
+          <img src={logoImg} alt="Rafaela Fotografias"/>
+
+        </header>
+        
+        <main>
+          <h1>Seja bem vindo(a) ao atendimento inicial online</h1>
+          <p>É possivel se retirar da plataforma a qualquer momento. 
+            O ensaio só será confirmado por você no final do site.</p>
+          <a href="/">Continuar</a>
+          <img src={bonecosImg} alt="Bonecos"/>
+        </main>
+
+        <footer>
+          <div className="perdido">
+            <img src={cercaImg} alt="Perdido?"/>
+            <h2>Está perdido? Caso seu desejo seja apenas 
+              se informar e não marcar o ensaio</h2>
+          </div>
+          
+          <div className="informacoes">
+            <p>Clique no ícone abaixo</p>
+            <i><AiOutlineArrowDown/></i>
+            <a href="/">Informacões</a>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
